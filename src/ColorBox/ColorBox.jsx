95507a9fcb65a5c908a4feb9b0a5cc044b10f581
Tony Nguyen <tony.nguyen@usonialabs.com>
2024-01-14 19:42:31 +0700

    add description box

--- src/ColorBox/ColorBox.jsx
+++ src/ColorBox/ColorBox.jsx
@@ -20,8 +20,31 @@ function ColorBox() {
 
   return (
     <>
+      <div>
+        <h2 className="mb-2 text-lg font-semibold text-gray-900">Requirements</h2>
+        <div className="space-y-1 text-gray-500 list-disc list-inside">
+          Build an playing color box. User can toggle switch color between boxes each other.
+          <ul className="space-y-1 text-gray-500 list-disc list-inside mt-5  ml-5">
+            <li>
+              We have 4 blocks: red, yellow, aqua, purple.
+            </li>
+            <li>
+              When user click red block. The other blocks will change same background color red.
+            </li>
+            <li>
+              After user click red block again. They will go back normal. 
+            </li>
+            <li>
+              Perform the action same with Gold, Aqua, Purple.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <br />
+
       <div className="mb-2">
-        Current color: {currentColor}
+        Current color: {currentColor || 'Default'}
       </div>
       <div className="boxColor">
         
