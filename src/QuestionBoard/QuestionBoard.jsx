a30138871c0f2b1cd637c109fd9bbe71fd492d90
Tony Nguyen <tony.nguyen@usonialabs.com>
2024-01-17 22:12:53 +0700

    fix question board

--- src/QuestionBoard/QuestionBoard.jsx
+++ src/QuestionBoard/QuestionBoard.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React from 'react';
+
+import { dataQuestions } from './dataQuestions';
+
+function fakeApi(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data)
+    }, 1000)
+  })
+}
 
 function QuestionBoard() {
+  React.useEffect(() => {
+    fakeApi(dataQuestions).then((data) => {
+      console.log(data)
+    })
+  })
+
   return (
-    <>
+    <div>
       <div>
         <h2 className="mb-2 text-lg font-semibold text-gray-900">Requirements</h2>
         <div className="space-y-1 text-gray-500 list-disc list-inside">
           Build a question board that displays the list question that group by categories.
           <ul className="space-y-1 text-gray-500 list-disc list-inside mt-5  ml-5">
             <li>
-              Show questions by categories.
+              Show questions by categories. Get data from dataQuestions.js
             </li>
             <li>
-              Show submissions question status: correct, incorrect, partially correct
+              Show submissions question status: correct, incorrect, partially correct. Get data from dataSubmissions.js
                 <ul className="text-gray-500 list- list-inside ml-10">
                   <li>
                     Correct answer: <span className="text-green-500">green</span>
@@ -31,7 +47,7 @@ function QuestionBoard() {
       <br />
       <br />
 
-      <div>
+      <div className='question_board'>
         <div className="column">
           <h2>HTML - 1 / 4</h2>
           <div className="boards">
@@ -73,7 +89,7 @@ function QuestionBoard() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
